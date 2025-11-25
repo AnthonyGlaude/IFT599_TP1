2963008f7c9106b2ec88f6969c866ace960753a8
AnthonyGlaude <anthony.glaude@usherbrooke.ca>
2025-11-24 22:55:51 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,21 @@
+"""
+TP2 – Analyse des données (IFT599 / IFT799)
+
+**Auteurs :**  
+-  Ana Karen Lopez Carbajal (lopa2603)
+-  Étienne Chaput (chae3018)
+-  Anthony Glaude (glaa3301)
+
+**Date de remise :** 25 novembre 2025  
+"""
+
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.impute import SimpleImputer
+
 import umap
+from sklearn.decomposition import PCA
 
 def load_hiseq_data(data_path, labels_path):
     """Charge et prépare les données Hi-Seq"""
@@ -28,16 +40,14 @@ def preprocess_hiseq(X, y):
     
     return X_scaled
 
-def reduce_dimension_umap(X, n_components=100, n_neighbors=15, min_dist=0.1):
-    
-    print(f"Reducing dimension from {X.shape[1]} to {n_components}D using UMAP...")
+def reduce_dimension_umap(X, n_components=2, n_neighbors=15, min_dist=0.1):
+    """Réduction de dimension avec UMAP"""
+    print(f"Reducing dimension to {n_components}D using UMAP...")
     reducer = umap.UMAP(n_components=n_components, n_neighbors=n_neighbors, min_dist=min_dist)
     X_reduced = reducer.fit_transform(X)
-    
     return X_reduced, reducer
 
 def reduce_dimension_acp(X, n_components=100):
-    
     print(f"Reducing dimension from {X.shape[1]} to {n_components}D using ACP...")
     pca = PCA(n_components=n_components)
     X_pca = pca.fit_transform(X)
